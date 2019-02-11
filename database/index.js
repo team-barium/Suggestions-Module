5df5f52361deb58b1af362f6ca74db426bcf0bbb
Taylor Bantle <bantle.taylor@gmail.com>
2019-02-11 11:31:37 -0800

    Updated database path

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/suggestions');
+mongoose.connect('mongodb://127.0.0.1/suggestions');
 
 var db = mongoose.connection;
 db.on('error', (err) => console.log('connection error: ', err));
