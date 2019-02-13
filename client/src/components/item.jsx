8110a3e052bb5dd448704c821bda68f36a20c499
Taylor Bantle <bantle.taylor@gmail.com>
2019-02-13 13:31:54 -0800

    Added hearts and extending window does not show more items

--- client/src/components/item.jsx
+++ client/src/components/item.jsx
@@ -2,33 +2,61 @@ import React from 'react';
 import StarRatings from 'react-star-ratings'
 import styles from '../styles/item.css';
 
-const Item = (props) => {
-	let price = `$${props.obj.price}`;
-	if (props.obj.salePrice) {
-		price = <div><span className={styles.red}>${props.obj.salePrice}</span> <span className={styles.strikethrough}> ${props.obj.price}</span></div>
-	}
-	let reviews = <div><StarRatings numberOfStars={5} starDimension="12px" rating={props.obj.reviewStars} starRatedColor='#767677' starEmptyColor="#eceeef" starSpacing="1px" /><span className={styles.ratingPad}>{props.obj.reviewsTotal}</span></div>;
-	if (!props.obj.reviewStars) {
-		reviews = null;
+class Item extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			filledHeart: 'https://s3-us-west-1.amazonaws.com/abibas-shoes/icons/heart-grey-filled.png',
+			outlinedHeart: 'https://s3-us-west-1.amazonaws.com/abibas-shoes/icons/heart-grey-outline.png',
+			currHeart: 'https://s3-us-west-1.amazonaws.com/abibas-shoes/icons/heart-grey-outline.png'
+		}
+		this.fillHeart = this.fillHeart.bind(this);
 	}
 
-return (
-	<div className={styles.wrapper} style={{order: props.order}}>
-		<div className={styles.item}>
-			<div id='picture'>
-				<img className={styles.picture} src={props.obj.productPicture}></img>
-			</div>
-			<div id='info' className={styles.text}>
-				<div className={styles.kind}>{props.obj.kind}</div>
-				<div className={styles.details}>
-					<div className={styles.title}>{props.obj.title}</div>
-					<div className={styles.price}>{price}</div>
+	fillHeart() {
+		console.log('heart clicked')
+		let {currHeart} = this.state;
+		if (currHeart === this.state.outlinedHeart) {
+			this.setState({currHeart: this.state.filledHeart})
+		} else {
+			this.setState({currHeart: this.state.outlinedHeart})
+		}
+	}
+
+	render() {
+		let price = `$${this.props.obj.price}`;
+		if (this.props.obj.salePrice) {
+			price = <div><span className={styles.red}>${this.props.obj.salePrice}</span> <span className={styles.strikethrough}> ${this.props.obj.price}</span></div>
+		}
+
+		let reviews = <div><StarRatings numberOfStars={5} starDimension="12px" rating={this.props.obj.reviewStars} starRatedColor='#767677' starEmptyColor="#eceeef" starSpacing="1px" /><span className={styles.ratingPad}>{this.props.obj.reviewsTotal}</span></div>;
+		if (!this.props.obj.reviewStars) {
+			reviews = null;
+		}
+
+		return (
+			<div className={styles.wrapper} style={{ order: this.props.order }}>
+				<div className={styles.item}>
+					<div id='picture' >
+						<img className={styles.picture} src={this.props.obj.productPicture}></img>
+						<div className={styles.heartWrapper}>
+							<img className={styles.heart} onClick={this.fillHeart} src={this.state.currHeart}></img>
+						</div>
+					</div>
+					<div id='info' className={styles.text}>
+						<div className={styles.kind}>{this.props.obj.kind}</div>
+						<div className={styles.details}>
+							<div className={styles.title}>{this.props.obj.title}</div>
+							<div className={styles.price}>{price}</div>
+						</div>
+						<div className={styles.ratings}>{reviews}</div>
+					</div>
 				</div>
-				<div className={styles.ratings}>{reviews}</div>
 			</div>
-		</div>
-	</div>
-)
+		)
+	}
+
+
 }
 
 export default Item;
