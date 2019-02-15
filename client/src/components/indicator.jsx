9631a068b11ba292e825fe79f833b385c346220f
Taylor Bantle <bantle.taylor@gmail.com>
2019-02-15 12:24:55 -0800

    Carousel is responsive and item decals in correct spots

--- client/src/components/indicator.jsx
+++ client/src/components/indicator.jsx
@@ -3,14 +3,23 @@ import styles from '../styles/indicator.css';
 import Pip from './pip.jsx'
 
 const Indicator = (props) => {
-    const {position} = props;
-    return (
-        <div className={styles.nav}>
-            {Array.from({length: 4}, (pip, i) =>
-                <Pip key={i} isCurrent={(i*4)===position} index={i} changePosition={props.changePosition} />
-            )}
-        </div>
-    )
+	const { position } = props;
+
+	const prevArrow = () => position !== 0 ? (<a className={styles.prev} onClick={props.prevSlide}><div className={styles.prevText}>&#8735;</div></a>) : null;
+
+	const nextArrow = () => position < 12 ? (<a className={styles.next} onClick={props.nextSlide}><div className={styles.nextText}>&#8735;</div></a>) : null;
+
+	return (
+		<div>
+			<div>{prevArrow()}</div>
+			<div>{nextArrow()}</div>
+			<div className={styles.nav}>
+				{Array.from({ length: 4 }, (pip, i) =>
+					<Pip key={i} isCurrent={(i * 4) === position} index={i} changePosition={props.changePosition} />
+				)}
+			</div>
+		</div>
+	)
 }
 
 export default Indicator;
