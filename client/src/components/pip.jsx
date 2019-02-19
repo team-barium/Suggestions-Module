b3cb62a2ea1b3cb1be751d480fe99717174cdd9c
Taylor Bantle <bantle.taylor@gmail.com>
2019-02-19 11:37:33 -0800

    Pip hover works

--- client/src/components/pip.jsx
+++ client/src/components/pip.jsx
@@ -3,10 +3,6 @@ import styles from '../styles/pip.css';
 
 const Pip = (props) => {
 
-    // const pipStyle = {
-    //     height: `${props.isCurrent ? '100%' : '25%'}`
-    // }
-
     return (
         <li className={styles.li} onClick={() => props.changePosition(props.index)}>
             <span className={styles.dot} style={{height: `${props.isCurrent ? '100%' : '25%'}`}}></span>
